fc3ba106f0367b2ce63fab1be0f5079731b40a74
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2026-01-10 20:53:39 -0500

    version 0.4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -30,20 +30,34 @@ function init(){
 
     getE('fondo').style.width = ancho+'px'
     getE('fondo').style.height = alto+'px'
-
-    getE('container-wrap').className = 'container-wrap-initial'
+    getE('fondo-img').style.backgroundImage = 'url(./assets/images/fondo.jpg)'
 }
 
 var animacion_titulos = null
 var animacion_titulos_count = 0
+var animacion_titulo_btn = null
+
 function setAnimations(){
+    getE('container-wrap').className = 'container-wrap-initial'
     getE('w-fotografia').className = 'w-fotografia-on'
-    getE('fondo-aros').className = 'fondo-aros-on fondo1'
-    
+    getE('fondo-aros').className = 'fondo-aros-on fondo0'
+
     animacion_titulos = setInterval(function(){
         if(animacion_titulos_count==3){
-            clearInterval(animacion_titulos_count)
-            animacion_titulos_count = null
+            clearInterval(animacion_titulos)
+            animacion_titulos = null
+
+            getE('w-boton').className = 'boton-on'
+            animacion_titulo_btn = setTimeout(function(){
+                clearTimeout(animacion_titulo_btn)
+                animacion_titulo_btn = null
+
+                console.log("animando")
+                getE('w-boton').className = 'boton-onn'
+                getE('btn-comenzar').className = 'w-button w-button-enabled'
+                getE('btn-comenzar').setAttribute('onclick','clickComenzar()')
+            },750)
+            
         }else if(animacion_titulos_count==0){
             getE('w-titulo').className = 'animacion-caida-1'
         }else if(animacion_titulos_count==1){
@@ -59,7 +73,7 @@ var animacion_fondo = null;
 var animacion_container_wrap = null;
 
 function clickComenzar(){
-    getE('fondo-aros').className = 'fondo-aros-off'
+    getE('fondo-aros').className = 'fondo-aros-off fondo0'
     animacion_fondo = setTimeout(function(){
         clearTimeout(animacion_fondo)
         animacion_fondo = null
@@ -76,8 +90,6 @@ function clickComenzar(){
                 if(animacion_titulos_count==3){
                     clearInterval(animacion_titulos_count)
                     animacion_titulos_count = null
-
-                    
                 }else if(animacion_titulos_count==0){
                     getE('m-titulo').className = 'animacion-caida-1'
                 }else if(animacion_titulos_count==1){
@@ -107,4 +119,21 @@ function outUnityBtn(zona){
     var clases = String(unity.className).split(" ")
     clases[clases.length-1] = 'm-unity-out'
     unity.className = clases.join(" ")
+}
+
+function clickUnityBtn(uni){
+    getE('fondo-aros').className = 'fondo-aros-off fondo1'
+    animacion_fondo = setTimeout(function(){
+        clearTimeout(animacion_fondo)
+        animacion_fondo = null
+
+        getE('container-wrap').className = 'content-wrap-1-1'
+        animacion_container_wrap = setTimeout(function(){
+            clearTimeout(animacion_container_wrap)
+            animacion_container_wrap = null
+
+            getE('fondo-aros').className = 'fondo-aros-on fondo2'
+            
+        },2000)
+    },500)
 }
